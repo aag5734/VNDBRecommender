70c6fa7006906065a759b01e3bb1e6b8dc8bd054
aag5734 <abrahamgonzalez3764@gmail.com>
2023-06-01 14:40:33 -0700

    add: prime react components

--- vn-rec/client/src/App.js
+++ vn-rec/client/src/App.js
@@ -1,15 +1,30 @@
 import rena from './assets/rena.jpg';
 import React, {useState} from "react";
+import {useFormik} from "formik"
 import './App.css';
+import {InputText} from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+import "primereact/resources/primereact.min.css";
+import 'primeicons/primeicons.css';                                  
 
 function App() {
-  const [username, setUsername] = useState("");
-
-  const handleSubmit = (e) => {
-    console.log(username);
+  const formSubmit = (e) => {
     // e.preventDefault();
-    setUsername("");
+    console.log(formik.values.username);
+    // setUsername("");
   }
+
+  const githubRedirect = (e) => {
+    window.location.href = "https://github.com/aag5734/VNDBRecommender";
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      username:''
+    },
+    onSubmit: formSubmit
+  });
 
   return (
     <div className="vndb">
@@ -18,7 +33,22 @@ function App() {
         <h1>
           Visual Novel Recommender
         </h1>
-        <form className="userForm" onSubmit={handleSubmit}>
+        <i className='pi pi-github' style={{ fontSize: '2.0rem' }} onClick={()=>{githubRedirect()}}></i>
+        <form onSubmit={formik.handleSubmit}>
+          <br/>
+          <label htmlFor='username'>username</label>
+          <span className="p-float-label">
+            <InputText
+              id="username"
+              name="username"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.username}
+            />
+          </span>
+          <Button type="submit">Submit</Button>
+        </form>
+        {/* <form className="userForm" onSubmit={handleSubmit}>
           <label htmlFor='username'>Username</label>
           <input
             type='text'
@@ -27,7 +57,8 @@ function App() {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-        </form>
+          <button className='userSubmit'>Enter</button>
+        </form> */}
       </header>
     </div>
   );
